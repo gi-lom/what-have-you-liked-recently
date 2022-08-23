d183b338eae3c5939852cc05f2962d2edac194cc
gi-lom <gl.rvg.2013@protonmail.com>
2022-08-23 13:38:00 +0200

    Adding catch in error page

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -4,27 +4,31 @@ import Header from "./header.jsx";
 import '../style/pages/error.scss';
 
 const Error = (props) => {
-    return (
-        <div>
-
-                <Header headerClass={"header-not-welcome"} />
-                <div id="loader">
-                    <div id="error-button">
-                        <div id="error-button-text">
-                            {props.stats.error} <br />
-                            <span id="error-button-desc">Error</span>
+    try {
+        return (
+            <div>
+                    <Header headerClass={"header-not-welcome"} />
+                    <div id="loader">
+                        <div id="error-button">
+                            <div id="error-button-text">
+                                {props.stats.error} <br />
+                                <span id="error-button-desc">Error</span>
+                            </div>
                         </div>
                     </div>
-                </div>
         
-                <div id="error">
-                    <div id="errorDescription">
-                        Something bad happened. <br />
-                        Try reloading the page. If it still fails, wait some minutes and redo it. <br />
+                    <div id="error">
+                        <div id="errorDescription">
+                            Something bad happened. <br />
+                            Try reloading the page. If it still fails, wait some minutes and redo it. <br />
+                        </div>
                     </div>
-                </div>
-        </div>
-    ) 
+            </div>
+        )
+    }
+    catch (err) {
+        return <div />
+    }
 }
 
 export default Error
