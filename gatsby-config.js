120fcf83240b7b2e9b623585db288548295f11da
gi-lom <gl.rvg.2013@protonmail.com>
2022-10-10 13:47:22 +0200

    SEO optimization

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,30 @@
 module.exports = {
   siteMetadata: {
     title: `What Have You Liked Recently`,
+    description: 'Detailed statistics of the latest 50 songs you faved on Spotify',
+    siteUrl: 'https://whathaveyoulikedrecently.gatsbyjs.io',
   },
   plugins: [
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-charts-css',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-offline",
+    "gatsby-plugin-preload-fonts",
+    "gatsby-plugin-sitemap",
     {
       resolve: 'gatsby-plugin-sass',
       options: {
         useResolveUrlLoader: true
       }
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "https://whathaveyoulikedrecently.gatsbyjs.io",
+        sitemap: "https://whathaveyoulikedrecently.gatsbyjs.io/sitemap.xml",
+        resolveEnv: () => process.env.GATSBY_ENV,
+        policy: [{ userAgent: '*', allow: '/' }]
+      },
     }
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React from "react";
+import { Helmet } from "react-helmet";
 
 import setLocalStorage from "./../modules/spotify.jsx";
 import Welcome from "./welcome.jsx";
@@ -10,12 +11,11 @@ import Header from "./header.jsx";
 import "@fontsource/secular-one";
 import "@fontsource/shrikhand";
 import "@fontsource/raleway";
-import "@fontsource/alegreya/900-italic.css"
+import "@fontsource/alegreya/900-italic.css";
 
-import spotifyLogo from '../images/spotify.png'
+import spotifyLogo from "../images/spotify.png";
 
 class MainPart extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = { stats: null, forceRender: false };
@@ -23,40 +23,55 @@ class MainPart extends React.Component {
   }
 
   componentDidMount() {
-    setLocalStorage(this.state.stats === null)
-    .then((finalResult) => {
-      if (finalResult !== null)
-        this.state.stats = finalResult;
+    setLocalStorage(this.state.stats === null).then((finalResult) => {
+      if (finalResult !== null) this.state.stats = finalResult;
       this.setState({ state: this.state });
     });
   }
 
   logout() {
-    if (window.confirm("This will log you out of Spotify in the whole browser. Continue?")) {
-      localStorage.clear()
-      sessionStorage.clear()
-      const url = 'https://accounts.spotify.com/logout'
-      const spotifyLogoutWindow = window.open(url, 'Spotify Logout', 'width=700,height=500,top=40,left=40')
-      const timeout = Promise.resolve(setTimeout(() => spotifyLogoutWindow.close(), 500))
-      timeout.then(() => { this.forceUpdate() } )
+    if (
+      window.confirm(
+        "This will log you out of Spotify in the whole browser. Continue?"
+      )
+    ) {
+      localStorage.clear();
+      sessionStorage.clear();
+      const url = "https://accounts.spotify.com/logout";
+      const spotifyLogoutWindow = window.open(
+        url,
+        "Spotify Logout",
+        "width=700,height=500,top=40,left=40"
+      );
+      const timeout = Promise.resolve(
+        setTimeout(() => spotifyLogoutWindow.close(), 500)
+      );
+      timeout.then(() => {
+        this.forceUpdate();
+      });
     }
   }
 
   render() {
-    let authState = typeof window !== 'undefined' ? localStorage.getItem('spotify_auth_state') : null
-    if (authState === null || window.location.href.split('#').length == 1)
-      return (
-        <Welcome />
-      )
+    let authState =
+      typeof window !== "undefined"
+        ? localStorage.getItem("spotify_auth_state")
+        : null;
+    if (authState === null || window.location.href.split("#").length == 1)
+      return <Welcome />;
     else {
-      let el = <div />
+      let el = <div />;
       if (this.state.stats === null)
-        el = <Loading forceRender={this.forceRender} />
+        el = <Loading forceRender={this.forceRender} />;
       else {
-      if (Object.keys(this.state.stats).includes("error"))
-        el = <Error stats={this.state.stats} forceRender={this.forceRender} />
-      else
-          el = <Stats stats={this.state.stats} forceRender={this.forceRender} />
+        if (Object.keys(this.state.stats).includes("error"))
+          el = (
+            <Error stats={this.state.stats} forceRender={this.forceRender} />
+          );
+        else
+          el = (
+            <Stats stats={this.state.stats} forceRender={this.forceRender} />
+          );
       }
       return (
         <>
@@ -71,27 +86,46 @@ class MainPart extends React.Component {
           </div>
           {el}
         </>
-      )
+      );
     }
   }
-
 }
 
 // markup
 const IndexPage = () => {
-
   return (
     <main>
-      <title>What Have You Liked Recently?</title>
+      <Helmet htmlAttributes={{ lang: "en" }}>
+        <title>What Have You Liked Recently?</title>
+        {props.robots == "nofollow" && (
+          <link rel="canonical" href="https://whathaveyoulikedrecently.gatsbyjs.io" />
+        )}
+        <meta charset="utf-8" />
+        <meta
+          name="description"
+          content="Detailed statistics of the latest 50 songs you faved on Spotify"
+        />
+        <meta
+          name="keywords"
+          content="spotify, music, spotify data, spotify library stats, faved tracks stats"
+        />
+        <meta name="author" content="gi-lom" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content={props.robots} />
+      </Helmet>
 
       <MainPart />
 
       <footer>
-        Released under GPL3 license<br />
-        Author: gi-lom - <a href="https://github.com/gi-lom/what-have-you-liked-recently">Github page</a>
+        Released under GPL3 license
+        <br />
+        Author: gi-lom -{" "}
+        <a href="https://github.com/gi-lom/what-have-you-liked-recently">
+          Github page
+        </a>
       </footer>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
